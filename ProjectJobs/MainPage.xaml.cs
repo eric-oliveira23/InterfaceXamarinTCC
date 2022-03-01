167b5775f26d2f908285383ef85458b07dda33d8
Eric <ericpoliveira23@gmail.com>
2022-02-28 22:30:36 -0300

    Adicionar arquivos de projeto.

---
+++ ProjectJobs/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectJobs
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Click_LogarAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+
+        async void Click_RegistrarAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Register());
+        }
+
+    }
+}
